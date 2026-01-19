8db808ad5dd343e4ddf44939d57472f908a6dd8e
KiagusRafi <kiagusmuhammadrafi@gmail.com>
2026-01-19 23:10:14 +0700

    TERAKHIR

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // in production, there's no localhost so we have to make this dynamic.
-const BASE_URL  = import.meta.env.MODE === "development" ? "http://localhost:6767/api" : "/api"
+const BASE_URL  = import.meta.env.MODE === "development" ? "http://localhost:6767/api" : `${import.meta.env.API_URL}/api`
 // const BASE_URL  = "http://localhost:6767/api";
 const api = axios.create({
     baseURL: BASE_URL,
